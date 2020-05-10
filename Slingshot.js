7a9e153e7a3e31f5782c9b8e2ec55667a8bb95c9
Ananya2306 <amitarora_1@yahoo.com>
2020-05-10 12:01:04 +0530

    RubberBand

--- Slingshot.js
+++ Slingshot.js
@@ -6,7 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.slingImage1 = loadImage("sprites/sling1.png");
+        this.slingImage2 = loadImage("sprites/sling2.png");
+        this.slingImage3 = loadImage("sprites/sling3.png");
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +19,27 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingImage1,200,20);
+        image(this.slingImage2,170,22);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push(); 
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(color(48,22,8));
+             if(pointA.x<220) {   
+
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            image(this.slingImage3,pointA.x-30,pointA.y-10,15,30);
+             }
+            else{
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+            image(this.slingImage3,pointA.x-30,pointA.y-10,15,30);
+        
+              pop(); 
+            }
         }
     }
     
